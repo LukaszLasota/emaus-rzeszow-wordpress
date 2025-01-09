9027d6a4c7bfb8b37db2523fb2e6b68c1a99989f
Łukasz Lasota <lukaszlasota89r@gmail.com>
2025-01-09 15:58:20 +0100

    Changes in accordion block

--- wp-content/plugins/custom-block-package/build/blocks/accordion-item/index.asset.php
+++ wp-content/plugins/custom-block-package/build/blocks/accordion-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '52de89fec14f8a474111');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '03ecb1044d7949e857f8');
