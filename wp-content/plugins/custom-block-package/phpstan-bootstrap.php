03a64b5e067a27b85f09518ae934cebd701353c1
Łukasz Lasota <lukaszlasota89r@gmail.com>
2026-02-16 21:39:48 +0100

    Add PHPStan and PHPCS configuration

---
+++ wp-content/plugins/custom-block-package/phpstan-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PHPStan Bootstrap File
+ *
+ * Defines plugin constants for static analysis only.
+ * These values are placeholders - not used at runtime.
+ *
+ * @package CustomBlockPackage
+ */
+
+define( 'UP_PLUGIN_DIR', __DIR__ . '/' );
+define( 'UP_PLUGIN_FILE', __DIR__ . '/index.php' );
+define( 'UP_PLUGIN_URL', '/wp-content/plugins/custom-block-package/' );
