9f637faeaba9df4bfb0688b08f769d5f04f3983f
Łukasz Lasota <lukaszlasota89r@gmail.com>
2026-03-05 21:55:17 +0100

    Show hover effect by default on mobile for image-text block
    On screens ≤1024px the gradient overlay and caption are always visible
    since hover is not available on touch devices.

--- wp-content/plugins/custom-block-package/build/blocks/image-text/index.asset.php
+++ wp-content/plugins/custom-block-package/build/blocks/image-text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '852cd6c67bd198c6885d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '7c95a7f0c861b64d8f1b');
